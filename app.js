9e390f48635040ecc7a4e6ccfb24727f125caf10
Fabian Venetz <fabian.venetz@gmail.com>
2014-04-01 17:21:50 +0200

    eliminate space

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
 function ParentCtrl($scope){
- $scope.foo = "Hello";
-
- //Emit to Parent example part 2
- $scope.$on("UPDATE_PARENT", function(event, message){
-   $scope.foo = message;
-
-
-   //Broadcast to Child example part 1
-   $scope.$broadcast("DO_BIDDING", {
-     buttonTitle : "Taken over",
-     onButtonClick : function(){
-       $scope.foo = "HAHA this button no longer works!";
-     }
-   });
-
-
- });
+  $scope.foo = "Hello";
+  //Emit to Parent example part 2
+  $scope.$on("UPDATE_PARENT", function(event, message){
+    $scope.foo = message;
+    //Broadcast to Child example part 1
+    $scope.$broadcast("DO_BIDDING", {
+      buttonTitle : "Taken over",
+      onButtonClick : function(){
+        $scope.foo = "HAHA this button no longer works!";
+      }
+    });
+  });
 }
 
 function ChildCtrl($scope){
   $scope.buttonTitle = "Update Parent";
   $scope.onButtonClick = function(){
-
     //Emit to Parent example part 1
     this.$emit("UPDATE_PARENT", "Updated");
   };
-
   //Broadcast to Child example part 2
   $scope.$on("DO_BIDDING", function(event, data){
     for(var i in data){
       $scope[i] = data[i];
     }
   });
-
 }
